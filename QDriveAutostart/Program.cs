82aad22094cda48ef5afeb4a3194c44ce3fae6c5
Tobias Hattinger <tobias.hattinger@gmx.at>
2020-08-19 02:17:59 +0200

    Updated NuGet-packages (including WrapSQL)

---
+++ QDriveAutostart/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QDriveAutostart
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try 
+            { 
+                Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzAwODQzQDMxMzgyZTMyMmUzMEorcUU5VnUwTldFTGdoMkNaR0xJeW9CR0huRXhsQnZZWlVaWWkxaVBsOFE9");
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new QDrive());
+            }
+            catch
+            {
+                MessageBox.Show("Q-Drive encountered an unecpected error and needs to be shut down.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ QDriveManager/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace QDriveManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {  
+                Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzAwODQzQDMxMzgyZTMyMmUzMEorcUU5VnUwTldFTGdoMkNaR0xJeW9CR0huRXhsQnZZWlVaWWkxaVBsOFE9");
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new QDriveManager());
+            }
+            catch
+            {
+                MessageBox.Show("Q-Drive encountered an unecpected error and needs to be shut down.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
